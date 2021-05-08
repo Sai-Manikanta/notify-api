b5b13be2eb372443b07754ae4461bc060f786d3f
mani333007 <mani333007@gmail.com>
2021-05-08 20:28:10 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+var cors = require('cors')
+const Vonage = require('@vonage/server-sdk')
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello world!")
+})
+
+
+// nexmo sms code when hit get resquest to /notify
+const vonage = new Vonage({
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRETE
+});
+
+const from = "Vonage APIs"
+const to = "919505629940"
+const text = 'Hello World!'
+
+app.get('/notify', (req, res) => {
+     vonage.message.sendSms(from, to, text, (err, responseData) => {
+        if (err) {
+             res.status(200).json([{ status: "error" }])
+        } else {
+            if(responseData.messages[0]['status'] === "0") {
+                 res.status(200).json([{ status: "success" }])
+             } else {
+                 ///console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+                 res.status(200).json([{ status: "error" }])
+             }
+         }
+     })
+})
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
